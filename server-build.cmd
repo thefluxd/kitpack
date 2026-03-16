57eed3361adaa9482b4398167fa38d33a76a1a76
fluxd <thefluxd@proton.me>
2026-03-16 23:46:21 +0100

    update server-build file

--- server-build.cmd
+++ server-build.cmd
@@ -1,3 +1,15 @@
+@echo off
+REM Change to the directory of the script
+cd /d "%~dp0"
+
+REM save original directory
+set "originalDir=%CD%"
+
+rmdir /s /q server-output
 mkdir server-output
 cd server-output
+
 java -jar ../build-tools/packwiz-installer-bootstrap.jar -g -s server ../pack.toml
+
+REM go back
+cd "%originalDir%"
